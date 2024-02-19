38df4b06e12d30aeb56174bb6539e62d5521b134
weera <weera@10.72.12.157>
2024-02-19 11:50:29 +0700

    Testtt

--- src/testt.java
+++ src/testt.java
@@ -1,12 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author weera
- */
 public class testt {
-    
+    public static void main(String[] args) {
+        System.out.println("Deez nuts");
+    }
 }
