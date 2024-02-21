6adf6b0b2938da03e66679cf8ea1c9ed8abf4c31
YoSirawit <66070314@kmitl.ac.th>
2024-02-21 23:12:44 +0700

    test login function

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
     public static void main(String[] args) {
         LoginManager lm = new LoginManager();
         if(lm.login("admin", "1234")){
-        System.out.println("Username : " + lm.getAccount().getUsername());
-        System.out.println("FirstName : " + lm.getAccount().getFirstName());
-        System.out.println("LastName : " + lm.getAccount().getLastName());
-        System.out.println("Level : " + lm.getAccount().getLevel());
+            System.out.println("Username : " + lm.getAccount().getUsername());
+            System.out.println("FirstName : " + lm.getAccount().getFirstName());
+            System.out.println("LastName : " + lm.getAccount().getLastName());
+            System.out.println("Level : " + lm.getAccount().getLevel());
+        } else {
+            System.out.println("No username or wrong password");
         }
     }
 }
