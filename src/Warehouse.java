7ac1d7619c31fe22bb526e5a63dd33a4d1d09a59
Sirachet Chotthakunanan <sirachet.c@gmail.com>
2024-02-28 12:00:24 +0700

    Warehouse V.1

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,10 +1,12 @@
 import java.sql.*;
 
 public class Warehouse extends Online{
+    private String ID;
     private String name;
     private String type;
     private String Brand;
     private Double price;
+    private int volume;
 
     public Warehouse(){
     try{
@@ -17,13 +19,28 @@ public class Warehouse extends Online{
         
     }
     public void addItems(String type, String Brand, String name, Double price, int volume){
-        
+        try{
+            String add = "INSERT INTO inventory(item_type="+type+", brand="+Brand+", item_name="+name+", price="+price+", quantity="+volume+")";
+            this.getStatement().executeUpdate(add);
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
     public void removeItems(String name, int volume) {
-        String del = "DELETE FROM inventory WHERE quantity=" + volume;
-        this.getStatement().executeUpdate(del);
+        try{
+            String count = "SELECT quantity FROM inventory WHERE item_name="+name;
+            int count1 = Integer.parseInt(count) - volume;
+            String del = "DELETE FROM inventory (item_name, quantity) WHERE quantity="+ count1;
+            this.getStatement().executeUpdate(del);
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
     public void removeItems(int ID, int volume){
-        
+        try{
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
