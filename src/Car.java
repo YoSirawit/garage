c4693046cd5df2c924a15bba103c242f0ac9e91d
YoSirawit <66070314@kmitl.ac.th>
2024-03-13 18:47:55 +0700

    Modified Car class

--- src/Car.java
+++ src/Car.java
@@ -1,17 +1,40 @@
 
 public class Car {
+    public static final int STATUS_NOTWORKING = 0;
+    public static final int STATUS_WORKING = 1;
+    public static final int STATUS_FINISH = 2;
+    
     private String carOwner;
     private String carName;
     private String brand;
     private String carID;
     private int status;
-    public Car(String carOwner, String brand, String carName, String carID, int status) {
+    private Bill bill;
+    public Car(String carOwner, String brand, String carName, String carID, int status){
         this.carName = carName;
         this.carOwner = carOwner;
         this.brand = brand;
         this.carID = carID;
         this.status = status;
+        this.bill = null;
     }
+    
+    public void setCarOwner(String owner){
+        this.carOwner = owner;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setCarName(String name){
+        this.carName = name;
+    }
+    public void setCarID(String ID){
+        this.carID = ID;
+    }
+    public void setStatus(int status){
+        this.status = status;
+    }
+    
     public String getCarOwner() {
         return this.carOwner;
     }
@@ -26,5 +49,8 @@ public class Car {
     }
     public int getStatus() {
         return this.status;
+    }
+    public Bill getBill(){
+        return this.bill;
     }
 }
