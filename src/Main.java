936dc3d961be1277f96dfa95c134cbdfdc8b3a61
YoSirawit <66070314@kmitl.ac.th>
2024-03-21 20:30:51 +0700

    Change Main file

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
 
 public class Main {
     public static void main(String[] args) {
-        LoginManager lm = new LoginManager();
-        if(lm.login("admin", "1234")){
-            System.out.println("Username : " + lm.getAccount().getUsername());
-            System.out.println("FirstName : " + lm.getAccount().getFirstName());
-            System.out.println("LastName : " + lm.getAccount().getLastName());
-            System.out.println("Level : " + lm.getAccount().getLevel());
-        } else {
-            System.out.println("No username or wrong password");
-        }
+        new MainController();
     }
+    
+//    public static void main(String[] args) {
+//        LoginManager lm = new LoginManager();
+//        if(lm.login("admin", "1234")){
+//            System.out.println("Username : " + lm.getAccount().getUsername());
+//            System.out.println("FirstName : " + lm.getAccount().getFirstName());
+//            System.out.println("LastName : " + lm.getAccount().getLastName());
+//            System.out.println("Level : " + lm.getAccount().getLevel());
+//        } else {
+//            System.out.println("No username or wrong password");
+//        }
+//    }
 }
